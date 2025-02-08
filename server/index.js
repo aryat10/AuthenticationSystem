a97362455bb98edc62a28deb7e6cd5e9ef201f87
Aryat <aryatsrishadow007@gmail.com>
2025-02-08 19:02:57 +0530

    Configure Express server with CORS and JSON middleware

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const app = express();
 const port = 4000;
 
+
+app.use(cors());
+app.use(express.json());
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
